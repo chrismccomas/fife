aaeb97e7ba2ca06a61e002f1c8c8334bc099d255
Chris McComas <chrismccomas@ucwv.edu>
2010-02-09 15:13:54 -0500

    Changed Django Project and App Name 02-09-10

--- urls.py
+++ urls.py
@@ -1,12 +1,13 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template, redirect_to
-from django.views.generic import list_detail
-
+ 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	(r'^', include('ucwv.fife.urls')),
+
+	(r'^', include('fife.courses.urls')),
+	
 	(r'^admin/', include(admin.site.urls)),
 	#(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
